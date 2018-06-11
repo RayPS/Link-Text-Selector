28009a71f4b374ee07b126e26606f0fccbc7d19b
Ray <ray@rayps.com>
2018-06-11 14:04:15 +0800

    Clean and reformat code

--- LinkTextSelector.safariextension/inject.js
+++ LinkTextSelector.safariextension/inject.js
@@ -1,95 +1,94 @@
 if (window.top === window){
-  // Not an iframe
+    // Not an iframe;
 
-  var hotKey = undefined
+    var hotKey = undefined;
 
-  safari.self.tab.dispatchMessage("askForHotKey");
+    this.safari.self.tab.dispatchMessage('askForHotKey');
 
-  safari.self.addEventListener("message", function (Event) {
-    if (Event.name == "answerForHotKey") {
-      hotKey = Event.message || "Shift"
-    }
-    if (Event.name == "changedForHotKey") {
-      // hotKey = Event.message
-      // console.log("hotkey changed to " + hotKey)
-    }
-  }, false)
-  
+    this.safari.self.addEventListener('message', function (Event) {
+        if (Event.name == 'answerForHotKey') {
+            hotKey = Event.message || 'Shift';
+        }
+        if (Event.name == 'changedForHotKey') {
+            // hotKey = Event.message;
+            // console.log('hotkey changed to ' + hotKey);
+        }
+    }, false);
+    
 
-  document.addEventListener('keyup', (event) => {
-    if (event.key == hotKey) {
-      let target = document.querySelector("a:hover")
-      if (target) {
-        let box = makeBoxElement(target)
-        document.body.appendChild(box)
-        box.show()
-      }
-      console.log(hotKey);
-      
-    }
-  })
+    document.addEventListener('keyup', (event) => {
+        if (event.key == hotKey) {
+            let target = document.querySelector('a:hover');
+            if (target) {
+                let box = makeBoxElement(target);
+                document.body.appendChild(box);
+                box.show();
+            }
+        }
+    });
 }
 
 
-function makeBoxElement(element) {    
-  let offset = 8
-  
-  let div = document.createElement("div")
-  div.innerHTML = element.innerHTML
-  
-  let rect = element.getBoundingClientRect()
-  let expandedBounding = {
-    left: rect.left - offset + window.scrollX,
-    top: rect.top - offset + window.scrollY,
-    width: rect.width + offset * 2,
-    height: rect.height + offset * 2
-  }
-  for (let key in expandedBounding) {
-    expandedBounding[key] += "px"
-  }
+function makeBoxElement(element) {
+    let offset = 8;
 
-  let computedStyle = window.getComputedStyle(element)
-  
-  let style = {
-    "position": "absolute",
-    "z-index": 2147483647, // Literally the maximum number LOL..
-    "background-color": "white",
-    "font": computedStyle.font,
-    "color": "black",
-    "padding-left": offset + parseInt(computedStyle.paddingLeft) + "px",
-    "padding-top": offset + parseInt(computedStyle.paddingTop) + "px",
-    "box-sizing": "border-box",
-    "border-radius": "4px",
-    "box-shadow": "0px 0px 0px 1px rgba(0 ,0, 0, 0.1), 0px 4px 8px rgba(0 ,0, 0, 0.25), 0px 8px 16px rgba(0 ,0, 0, 0.25)",
-    "cursor": "text",
-    "transition": "transform 0.1s ease-out, opacity 0.1s ease-in",
-    "transform": "scale(1)"
-  }
-  style = Object.assign(style, expandedBounding)
+    let div = document.createElement('div');
+    div.innerHTML = element.innerHTML;
 
-  for (let key in style) {
-    div.style.setProperty(key, style[key], "important")
-  }
+    let rect = element.getBoundingClientRect();
+    let expandedBounding = {
+        left: rect.left - offset + window.scrollX,
+        top: rect.top - offset + window.scrollY,
+        width: rect.width + offset * 2,
+        height: rect.height + offset * 2
+    };
+    for (let key in expandedBounding) {
+        expandedBounding[key] += 'px';
+    }
 
-  div.querySelectorAll("*").forEach( x => {
-    x.style.cursor = "text"
-  })
+    let computedStyle = window.getComputedStyle(element);
 
-  div.show = function(){
-    setTimeout(function(){
-      div.style.transform = "scale(1.1)"
-      setTimeout(function(){
-        div.style.transform = "scale(1)"
-      },100)
-    },50)
-  }
+    let style = {
+        'position': 'absolute',
+        'z-index': 2147483647, // Literally the maximum number LOL..;
+        'background-color': 'white',
+        'font': computedStyle.font,
+        // 'line-height': computedStyle.lineHeight,
+        'color': 'black',
+        'padding-left': offset + parseInt(computedStyle.paddingLeft) + 'px',
+        'padding-top': offset + parseInt(computedStyle.paddingTop) + 'px',
+        'box-sizing': 'border-box',
+        'border-radius': '4px',
+        'box-shadow': '0px 0px 0px 1px rgba(0 ,0, 0, 0.1), 0px 4px 8px rgba(0 ,0, 0, 0.25), 0px 8px 16px rgba(0 ,0, 0, 0.25)',
+        'cursor': 'text',
+        'transition': 'transform 0.1s ease-out, opacity 0.1s ease-in',
+        'transform': 'scale(1)'
+    };
+    style = Object.assign(style, expandedBounding);
 
-  div.addEventListener("mouseleave", function(){
-    div.style.opacity = 0
-    setTimeout(function(){
-      div.parentNode.removeChild(div)
-    },100)
-  })
+    for (let key in style) {
+        div.style.setProperty(key, style[key], 'important');
+    }
 
-  return div
+    div.querySelectorAll('*').forEach( x => {
+        x.style.cursor = 'text';
+    });
+
+    div.show = function(){
+        setTimeout(function(){
+            div.style.transform = 'scale(1.1)';
+            setTimeout(function(){
+                div.style.transform = 'scale(1)';
+            },100);
+        },50);
+    };
+
+    div.addEventListener('mouseleave', function(){
+        div.style.opacity = 0;
+        setTimeout(function(){
+            div.parentNode.removeChild(div);
+        },100);
+    });
+
+    return div;
 }
