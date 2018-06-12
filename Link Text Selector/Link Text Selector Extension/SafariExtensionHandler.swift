66159da189447deb2d60d043652390244e7ea456
Ray <ray@rayps.com>
2018-06-12 11:45:17 +0800

    Remove validate toolbar item

--- Link Text Selector/Link Text Selector Extension/SafariExtensionHandler.swift
+++ Link Text Selector/Link Text Selector Extension/SafariExtensionHandler.swift
@@ -14,16 +14,8 @@ class SafariExtensionHandler: SFSafariExtensionHandler {
         // This method will be called when a content script provided by your extension calls safari.extension.dispatchMessage("message").
         page.getPropertiesWithCompletionHandler { properties in
             NSLog("The extension received a message (\(messageName)) from a script injected into (\(String(describing: properties?.url))) with userInfo (\(userInfo ?? [:]))")
+
+
         }
     }
-    
-    override func validateToolbarItem(in window: SFSafariWindow, validationHandler: @escaping ((Bool, String) -> Void)) {
-        // This is called when Safari's state changed in some way that would require the extension's toolbar item to be validated again.
-        validationHandler(true, "")
-    }
-    
-    override func popoverViewController() -> SFSafariExtensionViewController {
-        return SafariExtensionViewController.shared
-    }
-
 }
