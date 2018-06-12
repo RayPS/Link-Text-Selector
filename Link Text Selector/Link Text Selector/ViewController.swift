4484f07b06584aa349a7ac97ac62d7ec5e52d58c
Ray <ray@rayps.com>
2018-06-12 17:04:36 +0800

    Implement settings and userdefaults

--- Link Text Selector/Link Text Selector/ViewController.swift
+++ Link Text Selector/Link Text Selector/ViewController.swift
@@ -14,15 +14,36 @@ class ViewController: NSViewController, NSWindowDelegate {
     @IBOutlet weak var radioButton_control: NSButton!
     @IBOutlet weak var radioButton_option: NSButton!
     @IBOutlet weak var radioButton_command: NSButton!
+
+    let keys = ["Shift", "Control", "Alt", "Meta"]
+
+    var userDefaults: UserDefaults?
+
+    var hotKey = String() {
+        didSet {
+            let radioButtons: [String: NSButton] = [
+                "Shift": radioButton_shift,
+                "Control": radioButton_control,
+                "Alt": radioButton_option,
+                "Meta": radioButton_command
+            ]
+
+            radioButtons[hotKey]?.state = .on
+
+            userDefaults?.set(hotKey, forKey: "hotKey")
+            userDefaults?.synchronize()
+        }
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        userDefaults = UserDefaults(suiteName: "group.Link-Text-Selector")
     }
 
     override func viewDidAppear() {
         view.window!.delegate = self
         view.window!.styleMask.remove(.resizable)
+        hotKey = userDefaults?.string(forKey: "hotKey") ?? keys.first!
     }
 
     func windowShouldClose(_ sender: NSWindow) -> Bool {
@@ -37,9 +58,8 @@ class ViewController: NSViewController, NSWindowDelegate {
     }
 
 
-
-    @IBAction func hotKeyRadioButtonSelected(_ sender: Any) {
-
+    @IBAction func hotKeyRadioButtonSelected(_ sender: NSMatrix) {
+        hotKey = keys[sender.selectedTag()]
     }
 }
 
