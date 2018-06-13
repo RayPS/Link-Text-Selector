c23138cc306f9ac1055801e8c8de81b48026e829
Ray <ray@rayps.com>
2018-06-13 12:23:29 +0800

    Add button to take user to the safari preferences

--- Link Text Selector/Link Text Selector/ViewController.swift
+++ Link Text Selector/Link Text Selector/ViewController.swift
@@ -18,6 +18,9 @@ class ViewController: NSViewController, NSWindowDelegate {
 
     @IBOutlet weak var enableIndicator: NSTextField!
     @IBOutlet weak var infomationLabel: NSTextField!
+    @IBOutlet weak var enableButton: NSButton!
+
+    let extId = "com.rayps.Link-Text-Selector.Link-Text-Selector-Extension"
 
     let keys = ["Shift", "Control", "Alt", "Meta"]
 
@@ -44,14 +47,14 @@ class ViewController: NSViewController, NSWindowDelegate {
         userDefaults = UserDefaults(suiteName: "group.Link-Text-Selector")
     }
 
-
+    override func viewWillAppear() {
+        checkExtensionState()
+    }
 
     override func viewDidAppear() {
         view.window!.delegate = self
         view.window!.styleMask.remove(.resizable)
         hotKey = userDefaults?.string(forKey: "hotKey") ?? keys.first!
-
-        enableIndicator.layer?.cornerRadius = 8
 
         Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(ViewController.checkExtensionState), userInfo: nil, repeats: true)
     }
@@ -68,16 +71,20 @@ class ViewController: NSViewController, NSWindowDelegate {
     }
 
     @objc func checkExtensionState() {
-        SFSafariExtensionManager.getStateOfSafariExtension(withIdentifier: "com.rayps.Link-Text-Selector.Link-Text-Selector-Extension") { (state, error) in
+        SFSafariExtensionManager.getStateOfSafariExtension(withIdentifier: extId) { (state, error) in
             DispatchQueue.main.async {
                 if let status = state?.isEnabled {
                     self.enableIndicator.textColor = status ? .systemGreen : .systemRed
                     self.infomationLabel.stringValue = status ? "Enabled" : "Extension is currently disabled, enable in Safari preferences"
+                    self.enableButton.isHidden = status
                 }
             }
         }
     }
 
+    @IBAction func enableButtonClicked(_ sender: Any) {
+        SFSafariApplication.showPreferencesForExtension(withIdentifier: extId)
+    }
 
     @IBAction func hotKeyRadioButtonSelected(_ sender: NSMatrix) {
         hotKey = keys[sender.selectedTag()]
